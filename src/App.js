32dc9730c3d5102adedda649cad30f2f730958c2
karthick <jk9165651@gmail.com>
2026-04-07 12:55:41 +0530

    kjgsadfk

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // ← HashRouter
 import BookDetails from "./pages/BookDetails";
 import Navbar from "./components/Navbar";
 import AddBook from "./pages/AddBook";
 import "./App.css";
 import EditBook from "./pages/EditBook";
-
-
-
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
+      {/* ← changed */}
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/add" element={<AddBook />} />
         <Route path="/book/:id" element={<BookDetails />} />
         <Route path="/edit/:id" element={<EditBook />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
